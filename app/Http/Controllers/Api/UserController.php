df5f81c78ce8fc193704867697fe38bb89f18ee4
Rui Anderson <ruiandersonsantos@gmail.com>
2017-11-26 15:17:10 -0200

    Implementando cadastro de usuario.

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -45,7 +45,7 @@ class UserController extends Controller
 
         $usuario = $request->all();
 
-        if($this.$this->existUsuario($usuario->email)){
+        if($this->existUsuario($usuario->email)){
             return response()->json(['error' => 'E-mail já existnte'],401);
         }
 
