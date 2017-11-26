d1ff02f47043bea17e5326161c88b85b5ff39e3b
Rui Anderson <ruiandersonsantos@gmail.com>
2017-11-26 16:12:01 -0200

    Implementando cadastro de usuario.

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -116,7 +116,7 @@ class UserController extends Controller
         $result = \App\Model\User::where('email','=',$email)->get();
 
 
-        dd($result);
+        dd(emptyArray($result));
         return isset($result['email']);
     }
 
